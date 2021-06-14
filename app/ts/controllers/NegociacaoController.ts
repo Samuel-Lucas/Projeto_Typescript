517a33a2488e4061178310684fd6b493d53aaaad
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-06-14 12:57:17 -0300

    Configurando método para armazenar as instâncias de Negociação

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData : HTMLInputElement;
     private _inputQuantidade : HTMLInputElement;
     private _inputValor : HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -21,6 +22,15 @@ class NegociacaoController {
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao); // Método para armazenar no array de negociações
+
+       /* this._negociacoes.paraArray().length = 0;
+        
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        });
+        */
     }
 }
